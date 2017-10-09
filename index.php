9f1778e2880be1964345187d1047376142d2afd2
kim yong ho <kyhwin2000@gmail.com>
2017-10-10 02:01:52 +0900

    거의 다 완성된 것 같았으나 이상한 버그가 남아있음…

--- index.php
+++ index.php
@@ -14,8 +14,67 @@
 	</body>
 </html> -->
 <?php
-$text = "사과 10개";
-print_r(parseText($text));
+$text = "사과 1개 바나나 3개";
+// print_r(parseText($text));
+$f_item = array();
+$f_name = array();
+$f_number = array();
+$f_unit = array();
+
+//DB 연결
+$db = new mysqli('localhost','root','Dntjd13!','dietbot');
+mysqli_query($db,"set names utf8");   
+if ( $db->connect_error ) exit('접속 실패 : '.$db->connect_error);
+
+//특수문자 제거 
+$text = str_replace( ",","",$text);     //쉼표
+$text = str_replace( "&","",$text);     //앤드
+$text = str_replace( "<br/>","",$text); //줄바꿈
+$text = str_replace( "." ,"",$text); //마침표
+$text = str_replace( "!" ,"",$text); //느낌표
+$text = str_replace( "?" ,"",$text); //물음표
+$text = str_replace( "$#34" ,"",$text); //큰 따옴표
+$text = str_replace( "&#39" ,"",$text); //작은 따옴표
+$text = str_replace( "+" ,"",$text); //더하기
+$text = str_replace( ":" ,"",$text); //콜론
+$text = str_replace( ";" ,"",$text); //세미콜론
+$text = str_replace( "^^" ,"",$text); //웃음표시
+$text = str_replace( "~" ,"",$text); //물결표시
+
+// 음식 DB에서 자연어 검색
+$f_query = "SELECT food_Name, MATCH (food_Name)
+              AGAINST ('$text' IN NATURAL LANGUAGE MODE) AS score
+              FROM foodCal WHERE MATCH (food_Name) AGAINST
+              ('$text' IN NATURAL LANGUAGE MODE) LIMIT 5";
+$f_result=mysqli_query($db, $f_query);
+$pos = array();
+// DB와 매칭되는 음식명 문자열 위치 찾기
+while($f_row = mysqli_fetch_array($f_result)){
+	array_push($pos,mb_strpos($text,$f_row['food_Name'],0,'UTF-8'));
+}      
+$pos = array_filter($pos);
+sort($pos);	
+print_r($pos);
+echo("<BR>");
+
+// 음식명 문자열 위치부터 그 다음 음식명 문자열 위치까지 잘라서 아이템 분리하기
+for($q=0;$q<count($pos)-1;$q++){
+	array_push($f_item,mb_substr($text,$pos[$q],$pos[$q+1]-$pos[$q]));	
+}
+array_push($f_item,mb_substr($text,$pos[count($pos)-1],strlen($text)-$pos[count($pos)-1]));
+
+print_r($f_item);
+echo("<BR>");
+// 한 아이템마다 함수에 던져서 음식명, 섭취량, 단위 추출하기
+for($r=0;$r<count($f_item);$r++){
+	array_push($f_name,parseText($f_item[$r])[0]);
+	array_push($f_number,parseText($f_item[$r])[1]);
+	array_push($f_unit,parseText($f_item[$r])[2]);
+}
+
+print_r($f_name);
+print_r($f_number);
+print_r($f_unit);
 
 function parseText($text){
 	//DB 연결
@@ -54,11 +113,10 @@ function parseText($text){
     case "numeric"  :
       // 문자열 중 숫자만 추출하여 섭취량으로 저장
       $number = preg_replace("/[^0-9]*/s", "", $text);
-      // 문자열 중 한글만 추출하여 단위로 저장
-      preg_match_all('/( [\x{AC00}-\x{D7AF}]{2})+/', $text, $match);
-      $unit = $match[0];
-      // $unit = strrchr($text,$number);
-      // $unit = mb_substr($unit,1,(strlen($unit)-1),'utf-8');
+      // 문자열 중 한글, 영문만 추출하여 단위로 저장
+      $pattern = '/([\xEA-\xED][\x80-\xBF]{2}|[a-zA-Z])+/';
+    	preg_match_all($pattern, $text, $match);
+    	$unit = implode('', $match[0]);
       break;
     case "string" :
       // 문자열을 잘라내서 숫자 DB에 있는지 검사하여 섭취량 추출
